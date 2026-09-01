0abfe2d3236c9ab2ba26d2db84e29a0a54e92781
rcamargopires-cmd <r.camargo.pires@gmail.com>
2026-09-01 18:22:26 -0300

    feat: enable unified stock and audit import

--- index.tsx
+++ index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './services/storeScopeAdapter';
 import './services/stockBatchAdapter';
+import './services/unifiedStockAuditAdapter';
 import './services/monthCycleAdapter';
 import App from './App';
 import ManagerTopNav from './components/ManagerTopNav';
 import OperationalTools from './components/OperationalTools';
 import TradeCheckShell from './components/TradeCheckShell';
 import MarketPresenceCorrectionShell from './components/MarketPresenceCorrectionShell';
+import UnifiedStockAuditNotice from './components/UnifiedStockAuditNotice';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -22,5 +24,6 @@ root.render(
     <OperationalTools />
     <TradeCheckShell />
     <MarketPresenceCorrectionShell />
+    <UnifiedStockAuditNotice />
   </React.StrictMode>
 );
