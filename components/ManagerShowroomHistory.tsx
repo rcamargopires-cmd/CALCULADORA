1de7934c83a404e8e097f40aff80d1f71c181ac1
rcamargopires-cmd <r.camargo.pires@gmail.com>
2026-09-18 17:45:54 -0300

    feat: enable editable customer dossier from atendimento history

--- components/ManagerShowroomHistory.tsx
+++ components/ManagerShowroomHistory.tsx
@@ -307,7 +307,7 @@ const ManagerShowroomHistory: React.FC<Props> = ({ user }) => {
         </div>
       </div>
     </div>}
-    {selectedCustomer && <CustomerAttendanceDossier selected={selectedCustomer} items={items} onClose={() => setSelectedCustomer(null)} />}
+    {selectedCustomer && <CustomerAttendanceDossier selected={selectedCustomer} items={items} user={user} onClose={() => setSelectedCustomer(null)} />}
   </>;
 };
 
